b58e6faebaa269a446640fc243881cd9a96d6dfd
ahtazaz <57252754+ahtazaz@users.noreply.github.com>
2020-10-28 01:48:41 -0700

    Create nqueen.py

---
+++ nqueen.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:23:14 2020
+
+@author: Ahtazaz
+"""
+
+n = int(input("Enter the number of Queens : "))
+Queen = [i for i in range(n)]
+
+def place(i, j):
+    for k in range(i):
+        if (Queen[k] == j or abs(Queen[k]-j) == abs(i-k)):
+            return False
+    return True
+
+def print_queen():
+    print("\nPlaces of Queens\n|", end="")
+    for i in range(n):
+        for j in range(Queen[i]):
+            print("  |", end="")
+        print("Q |", end="")
+        for j in range(n-Queen[i]-1):
+            print("  |", end="")
+        if(not i == n-1):
+            print("\n|", end="")
+    print()
+    
+def n_queen(i):
+    if i<n:
+        for j in range(n):
+            if(place(i, j)):
+                Queen[i] = j
+                if (i == n-1):
+                    print_queen()
+                else:
+                    n_queen(i+1)
+
+n_queen(0)
