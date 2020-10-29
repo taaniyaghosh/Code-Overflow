40cb6cae7db077630a105511c4d1ac187f73a665
jumokewale <51835853+jumokewale@users.noreply.github.com>
2020-10-29 18:47:51 +0100

    Update syntax

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,11 +1,11 @@
 # A function to check if a number is an odd or an even number
 def check_number(number):
-    if number % 2 == 0:
+    if (number % 2) == 0:
         print(f"The number {number} is an even number.")
     else:
         print(f"The number {number} is an odd number.")
 
         
 print("Check if your number is even or odd.")
-num = int(input("Enter a number."))
+num = int(input("Enter a number: "))
 check_number(num)
